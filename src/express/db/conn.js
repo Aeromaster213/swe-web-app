3252784eabcf9ae116177dcb17f0163a8bc7573e
Keshav Kumar Manjhi <96899605+Keshavv7@users.noreply.github.com>
2023-10-07 15:27:28 +0000

    Corrected cluster string

--- src/express/db/conn.js
+++ src/express/db/conn.js
@@ -1,3 +1,4 @@
+require("dotenv").config({ path: "../config.env" });
 const { MongoClient } = require("mongodb");
 const Db = process.env.ATLAS_URI;
 const client = new MongoClient(Db, {
@@ -13,7 +14,7 @@ module.exports = {
       // Verify we got a good "db" object
       if (db)
       {
-        _db = db.db("employees");
+        _db = db.db("accounts");
         console.log("Successfully connected to MongoDB."); 
       }
       return callback(err);
