c7ee8e20efafb80d0a8be9ab73ffb3779d304fdd
Abhinav <abhi.gupta8802@gmail.com>
2023-10-09 14:27:33 +0530

    About section redo

--- Frontend/src/Components/About/About.js
+++ Frontend/src/Components/About/About.js
@@ -1,4 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React, {useEffect, useState} from "react";
 import './about.css';
 
-export default function About() {}
+export default function About() {
+    return (
+        <div className={"about"}>
+            <p>We Use AI-Powered Tools</p>
+            <p>to convert media files</p>
+            <p>into text</p>
+        </div>
+    );
+}
