2f910b65601a704aedcbbb7afe43974251da0173
artemisia <arvind.srinivasn@gmail.com>
2023-10-09 23:58:59 +0530

    Clean up spurious files

--- ml.py
+++
@@ -1,8 +0,0 @@
-import torch, whisper
-import pathlib
-
-if __name__="__main__":
-    DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
-    model = whisper.load_model("medium", download_root="/persistent").to(DEVICE)
-    rlst = whisper.translate(model, "samples/gb0.ogg")
-    print(rlst["text"])
