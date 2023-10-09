725451228f24b39673fccff842caf6d96796dc2a
artemisia <arvind.srinivasn@gmail.com>
2023-10-10 00:34:45 +0530

    Node server is called now

--- src/express/ml.py
+++ src/express/ml.py
@@ -1,8 +1,14 @@
 import torch, whisper
 import pathlib
 
+def transcribe_srt(model, file):
+    rslt = whisper.transcribe(model, str(f))
+    return rslt["text"]
+
 if __name__=="__main__":
+    print("Model test loaded!")
     DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
     model = whisper.load_model("medium", download_root="/persistent").to(DEVICE)
-    rlst = whisper.transcribe(model, "samples/gb0.mkv")
-    print(rlst["text"])
+    for f in pathlib.Path("samples/").iterdir():
+        print(str(f))
+        print(transcribe_srt(model, f))
