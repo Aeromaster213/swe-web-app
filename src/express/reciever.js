1706d024bfc3bfafcbb6168a8ebbe28ff043f40d
Abhinav <abhi.gupta8802@gmail.com>
2023-10-10 01:04:11 +0530

    connecting the frontend and backend

--- src/express/reciever.js
+++ src/express/reciever.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 const crypto = require("crypto");
 
 const app = express();
-const port = 5000;
+const port = 3000;
 
 // Middleware to parse JSON and handle file uploads
 app.use(bodyParser.json());
