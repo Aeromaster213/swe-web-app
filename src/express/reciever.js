690ac7b1594150303b906b9d13ef8b0669d89cfd
Keshavv7 <cs21b027@iittp.ac.in>
2023-10-10 00:55:22 +0530

    Modified reciever.js

--- src/express/reciever.js
+++ src/express/reciever.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
+const path = require("path");
 const fs = require("fs");
 const crypto = require("crypto");
 
