58e4a65cd59a685e600521d3bf6f98d3ae0a961e
Keshavv7 <cs21b027@iittp.ac.in>
2023-10-10 06:26:23 +0530

    Minor fix

--- Backend/express/functionals/backpropagate.js
+++ Backend/express/functionals/backpropagate.js
@@ -6,22 +6,40 @@ const backport = 5001;
 
 app.use(cors());
 
+let server; // Define a variable to hold the server instance
 
 function sendToFrontend(data) {
-  // Assuming 'data' is an object with 'srt' and 'txt' properties
-  app.get("/api/results", (req, res) => {
-    res.json(data);
-  });
-
-  if (isCreated==false)
-  {
-    app.listen(backport, () => {
+  if (!server) {
+    server = app.listen(backport, () => {
       console.log(`Backpropagation server running on http://localhost:${backport}`);
     });
-    isCreated = true;
   }
+
+  app.get("/api/results", (req, res) => {
+    res.json(data);
+    // Close the server after sending the data
+    server.close(() => {
+      console.log(`Backpropagation server closed`);
+      server = null; // Reset the server variable
+    });
+  });
+}
+
+// function sendToFrontend(data) {
+//   // Assuming 'data' is an object with 'srt' and 'txt' properties
+//   app.get("/api/results", (req, res) => {
+//     res.json(data);
+//   });
+
+//   if (isCreated==false)
+//   {
+//     app.listen(backport, () => {
+//       console.log(`Backpropagation server running on http://localhost:${backport}`);
+//     });
+//     isCreated = true;
+//   }
   
 
-}
+// }
 
 module.exports = { sendToFrontend };
