b17564972e740050096118d1620a4916167adce5
Dhriti <cs21b019@iittp.ac.in>
2023-11-09 17:08:57 +0530

    Result page

--- Frontend/src/Components/Result/Result.js
+++ Frontend/src/Components/Result/Result.js
@@ -1,43 +1,28 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
+import { useUploadContext } from "../../Context/UploadContext";
 
 
 import "./result.css"; // Import the CSS file
-
+const graphic = require("./assets/graphic.png");
 
 
 export default function Result() {
-    const [strings, setStrings] = useState({}); // Initialize with an empty object
+    const { strings, fileName } = useUploadContext();
     const [copiedSrt, setCopiedSrt] = useState(false);
     const [copiedTxt, setCopiedTxt] = useState(false);
-
-
-    async function getResult() {
-        try {
-            const response = await fetch("http://localhost:5001/api/upload");
-            console.log(response);
-            return response;
-        } catch (error) {
-            console.log("Not Uploaded");
-            return error;
-        }
-    }
-
-    // useEffect(() => {
-    //     getResult().then((response) => response.json().then((data) => setStrings(data)))
-    // }, []);
+    const [data, setData] = useState({ srt: "srt", txt: "txt" });
 
     useEffect(() => {
-        getResult()
-            .then((response) => {
-                if (response.ok) {
-                    return response.json(); // Return the parsed JSON if response is ok
-                } else {
-                    throw new Error('Network response was not ok');
-                }
-            })
-            .then((data) => setStrings(data))
-            .catch((error) => console.error(error)); // Handle any errors
-    }, []);
+        console.log("Result component mounted");
+        console.log("strings:", strings);
+        if (strings) {
+            setData(strings);
+        }
+    }, [strings]);
+
+    useEffect(() => {
+        console.log("Result component mounted");
+    }, [strings]);
 
 
     useEffect(() => {
@@ -76,60 +61,57 @@ export default function Result() {
         }
     }, [copiedTxt]);
 
-    useEffect(() => {
-        return () => {
-            setStrings()
-        }
-    }, [])
 
     return (
         <div className="result">
-            {strings ? (
+            {data ? (
                 <div className="result-container">
-                    {/* <div className="result-box-wrap"> */}
-                        <div className="result-box">
-                            <div className="thumbnail"></div>
-                            <div className="details">
-                                <p>FILENAME: LOREM IPSUM</p>
-                                <p>FILESIZE: 25MB</p>
-                                <p>FILETYPE: AAC</p>
+                    <div className="result-info">
+                        <div className="infobox">
+                            <div className="info-thumbnail"></div>
+                            <div className="info-details">
+                                <p>FILENAME: {fileName}</p>
+                                {/* <p>FILESIZE: 25MB</p>
+                                <p>FILETYPE: AAC</p> */}
                             </div>
                         </div>
-                    {/* </div> */}
-                    <div className="result-heading">
-                        <p className="result-heading-text">YOUR TRANSCRIPT IS READY</p>
-                    </div>
-                    <div className="buttons">
-                        <div className="result-box txt-box">
-                            <button
-                                className="copy-button"
-                                onClick={() => copyTextToClipboard(strings.txt, "txt")}
-                            ></button>
-                            <p className="result-txt">COPY TXT</p>
-                            {strings.txt}
-                        </div>
-                        <div className="result-box srt-box">
-                            <button
-                                className="copy-button"
-                                onClick={() => copyTextToClipboard(strings.srt, "srt")}
-                            ></button>
-                            <p className="result-srt ">COPY SRT</p>
+
+                        <div className="result-heading">
+                            <p className="result-heading-text">
+                                Your Translation is ready!
+                            </p>
                         </div>
                     </div>
-                    <div className="result-display">
-                        <div className="copied-txt">
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    <div className="result-section">
+                        <div className="result-box result-srt">
+                            <div
+                                className="copy-button"
+                                onClick={() => copyTextToClipboard(data.srt, "srt")}
+                            >Copy Subtitle
+                            </div>
+                            <div className="result-res">
+                                <p className="result-text ">{data.srt}</p>
+                            </div>
                         </div>
-                        <div className="icon"></div>
-                        <div className="srtied-txt">
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                        <div className="result-graphic">
+                            <img src={graphic} alt="Graphic"></img>
+                        </div>
+                        <div className="result-box result-txt">
+                            <div
+                                className="copy-button"
+                                onClick={() => copyTextToClipboard(data.txt, "txt")}
+                            >Copy Text
+                            </div>
+                            <div className="result-res">
+                                <p className="result-text">{data.txt}</p>
+                            </div>
                         </div>
                     </div>
                 </div>
             ) : (
                 <h1 className="result-wait-text">Fetching The Results</h1>
             )
-            } 
+            }
         </div >
     );
 }
