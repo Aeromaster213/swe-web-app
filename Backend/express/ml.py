439292e0d521a134c8402b94f210f8c3ecaf7053
Arup Biswas <arup5abiswas@gmail.com>
2023-11-09 18:39:07 +0530

    allowing translation in ml.py

--- Backend/express/ml.py
+++ Backend/express/ml.py
@@ -32,3 +32,27 @@ if __name__=="__main__":
     for f in pathlib.Path("samples/").iterdir():
         print(str(f))
         print(transcribe_srt(model, f))
+
+def translation(in):
+    # List available models
+    torch.hub.list('pytorch/fairseq')  # [..., 'transformer.wmt16.en-de', ... ]
+
+    # Load a transformer trained on WMT'16 En-De
+    # Note: WMT'19 models use fastBPE instead of subword_nmt, see instructions below
+    en2de = torch.hub.load('pytorch/fairseq', 'transformer.wmt16.en-de',
+                        tokenizer='moses', bpe='subword_nmt')
+    en2de.eval()  # disable dropout
+
+    # The underlying model is available under the *models* attribute
+    #assert isinstance(en2de.models[0], fairseq.models.transformer.TransformerModel)
+
+    # Move model to GPU for faster translation
+    if(DEVICE == "cuda")
+        en2de.cuda()
+    # Translate a sentence
+    #en2de.translate('Hello world!')
+    # 'Hallo Welt!'
+
+    # Batched translation
+    #en2de.translate(['Hello world!', 'The cat sat on the mat.'])
+    # ['Hallo Welt!', 'Die Katze saß auf der Matte.']
