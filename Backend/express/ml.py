b2b77cd78c3633e982e73a0e8961dd5a456b9c38
artemisia <arvind.srinivasn@gmail.com>
2023-11-12 19:50:55 +0530

    SRT translation works as intended

--- Backend/express/ml.py
+++ Backend/express/ml.py
@@ -4,23 +4,25 @@ from os import path
 from torch import hub
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 hub.set_dir("/persistent/")
-models = {"de": 'transformer.wmt19.en-de', "ru": 'transformer.wmt19.en-ru'}
+models = {"de": 'transformer.wmt16.en-de', "fr": 'transformer.wmt14.en-fr'}
 
 def translation(inp, lang):
     if lang=="en": #We're doing En->X translations
         return inp
-    en2de = torch.hub.load('pytorch/fairseq', models[lang], checkpoint_file='model1.pt:model2.pt:model3.pt:model4.pt', tokenizer='moses', bpe='fastbpe')
+    en2de = torch.hub.load('pytorch/fairseq', models[lang], tokenizer='moses', bpe='subword_nmt')
     en2de.eval()  # disable dropout
     if DEVICE == "cuda":
         en2de.cuda()
     return en2de.translate(inp)
 
-def format_srt(transcript):
+def format_srt(transcript, lang):
     srt_output = ""
     for i, segment in enumerate(transcript['segments']):
         start = segment['start']
         end = segment['end']
         text = segment['text']
+        if lang!="en" and lang!="nl":
+            text=translation(text, lang)
 
         # Convert times to SRT format (hours:minutes:seconds,milliseconds)
         start_srt = f"{int(start // 3600):02d}:{int(start % 3600 // 60):02d}:{int(start % 60):02d},{int(start % 1 * 1000):03d}"
@@ -33,19 +35,18 @@ def format_srt(transcript):
 
 def remote(file, language):
     model = whisper.load_model("small", download_root="/persistent").to(DEVICE)
-    if language=="nl" or language=="en":
+    if language=="nl":
         rslt=whisper.transcribe(model, file)
         print("{\"txt\": \""+rslt["text"], end="\", ")
-        print("\"srt\": \""+format_srt(rslt), end="\"}")
+        print("\"srt\": \""+format_srt(rslt, "nl"), end="\"}")
     else:
-        options = whisper.DecodingOptions(task="translate")
-        rslt=whisper.decode(model, file, options)
+        rslt = model.transcribe(file)
         print("{\"txt\": \""+translation(rslt["text"], language), end="\", ")
-        print("\"srt\": \""+translation(format_srt(rslt), language), end="\"}")
+        print("\"srt\": \""+format_srt(rslt, language), end="\"}")
 
 if __name__=="__main__":
     print("Model test loaded!")
-    model = whisper.load_model("medium", download_root="/persistent").to(DEVICE)
+    model = whisper.load_model("small", download_root="/persistent").to(DEVICE)
     for f in pathlib.Path("samples/").iterdir():
         print(str(f))
         print(transcribe_srt(model, f))
