ecd280d8f080f3b544622b69f3eea7033ceaa182
Abhinav <abhi.gupta8802@gmail.com>
2023-11-14 00:19:47 +0530

    readme

--- Frontend/src/Components/Dashboard/Dashboard.js
+++ Frontend/src/Components/Dashboard/Dashboard.js
@@ -1,93 +1,122 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import "./dashboard.css";
-import {useUploadContext} from "../../Context/UploadContext";
+import { useUploadContext } from "../../Context/UploadContext";
 
 export default function Dashboard() {
-    const [transcriptions, setTranscriptions] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [copiedSrt, setCopiedSrt] = useState(false);
-    const [copiedTxt, setCopiedTxt] = useState(false);
-    const {username} = useUploadContext();
+  const [transcriptions, setTranscriptions] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [copiedSrt, setCopiedSrt] = useState(false);
+  const [copiedTxt, setCopiedTxt] = useState(false);
+  const { username } = useUploadContext();
 
-    useEffect(() => {
+  useEffect(() => {
+    setCopiedSrt(false);
+    setCopiedTxt(false);
+  }, [transcriptions]);
+
+  function copyTextToClipboard(text, type) {
+    const textarea = document.createElement("textarea");
+    textarea.value = text;
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand("copy");
+    document.body.removeChild(textarea);
+    type === "txt" ? setCopiedTxt(true) : setCopiedSrt(false);
+  }
+
+  useEffect(() => {
+    if (copiedSrt) {
+      const timer = setTimeout(() => {
         setCopiedSrt(false);
-        setCopiedTxt(false);
-    }, [transcriptions]);
+      }, 3000);
 
-    function copyTextToClipboard(text, type) {
-        const textarea = document.createElement("textarea");
-        textarea.value = text;
-        document.body.appendChild(textarea);
-        textarea.select();
-        document.execCommand("copy");
-        document.body.removeChild(textarea);
-        type === "txt" ? setCopiedTxt(true) : setCopiedSrt(false);
+      return () => clearTimeout(timer);
     }
+  }, [copiedSrt]);
 
-    useEffect(() => {
-        if (copiedSrt) {
-            const timer = setTimeout(() => {
-                setCopiedSrt(false);
-            }, 3000);
+  useEffect(() => {
+    if (copiedTxt) {
+      const timer = setTimeout(() => {
+        setCopiedTxt(false);
+      }, 3000);
+      return () => clearTimeout(timer);
+    }
+  }, [copiedTxt]);
 
-            return () => clearTimeout(timer);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          `http://localhost:5000/api/getDetails?user=${username}`
+        );
+        const data = await response.json();
+        const dataArray = Object.values(data);
 
-        }
-    }, [copiedSrt]);
+        console.log("data", JSON.stringify(dataArray));
+        setTranscriptions(dataArray);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
 
-    useEffect(() => {
-        if (copiedTxt) {
-            const timer = setTimeout(() => {
-                setCopiedTxt(false);
-            }, 3000);
-            return () => clearTimeout(timer);
+    fetchData();
+  }, [username]);
 
-        }
-    }, [copiedTxt]);
+  return (
+    <div className={"dashboard-container"}>
+      {loading ? (
+        <p className={"dashboard-loading"}>Loading...</p>
+      ) : transcriptions.length > 0 ? (
+        transcriptions.map((item, index) => (
+          <div className={"dashboard-card"} key={index}>
+            <div className={"dashboard-name dashboard-heading"}>
+              {item.file}
+            </div>
+            <div className={"dashboard-language dashboard-heading"}>
+              {(() => {
+                switch (item.language) {
+                  case "en":
+                    return "English";
+                  case "nl":
+                    return "Native";
+                  case "de":
+                    return "German";
+                  case "fr":
+                    return "French";
+                  default:
+                    return "Unknown";
+                }
+              })()}
+            </div>
 
-    useEffect(() => {
-        console.log(username);
-        const fetchData = async () => {
-            try {
-                const response = await fetch(`http://localhost:5000/api/getDetails?user=${username}`);
-                const data = await response.json();
-                const dataArray = Object.values(data);
+            <div className={"transcription-container"}>
+              <div className={"dashboard-srt"}>
+                <div
+                  className="copy-button"
+                  onClick={() => copyTextToClipboard(item.txt, "txt")}
+                >
+                  Copy Text
+                </div>
+                <div className="transcription-scroll">{item.srt}</div>
+              </div>
 
-                console.log("data", JSON.stringify(dataArray));
-                setTranscriptions(dataArray);
-            } catch (error) {
-                console.error('Error fetching data:', error);
-            } finally {
-                setLoading(false);
-            }
-        };
-
-        fetchData();
-    }, [])
-
-    return (<div className={"dashboard-container"}>
-            {loading ? (<p>Loading...</p>) : (transcriptions.map((item, index) => (<div className={"dashboard-card"}>
-                        <div className={"dashboard-name"}>{item.file}</div>
-                        <div className={"dashboard-language"}>{item.language}</div>
-
-                        <div className={"dashboard-srt"}>
-                            <div
-                                className="copy-button"
-                                onClick={() => copyTextToClipboard(item.txt, "txt")}
-                            >Copy Text
-                            </div>
-                            {item.srt}
-                        </div>
-
-                        <div className={"dashboard-txt"}>
-                            <div
-                                className="copy-button"
-                                onClick={() => copyTextToClipboard(item.srt, "srt")}
-                            >Copy Text
-                            </div>
-                            {item.txt}
-                        </div>
-                    </div>)))}
-
-        </div>)
+              <div className={"dashboard-txt"}>
+                <div
+                  className="copy-button"
+                  onClick={() => copyTextToClipboard(item.srt, "srt")}
+                >
+                  Copy Text
+                </div>
+                <div className="transcription-scroll">{item.txt}</div>
+              </div>
+            </div>
+          </div>
+        ))
+      ) : (
+        <p className={"dashboard-notrans"}>No Previous Transcriptions</p>
+      )}
+    </div>
+  );
 }
