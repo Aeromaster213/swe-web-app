d72780ed69156c1d7952b9274ddd24700ea245c4
Keshavv7 <cs21b027@iittp.ac.in>
2023-11-13 23:58:34 +0530

    Added JSDoc comments

--- Backend/express/functionals/cache_rename.js
+++ Backend/express/functionals/cache_rename.js
@@ -1,18 +1,30 @@
 const fs = require("fs");
 const path = require("path");
 
+
+/**
+ * Rename a file in the file cache directory.
+ *
+ * @param {string} originalFileName - The original file name.
+ * @param {string} newFileName - The new file name.
+ */
 function renameFile(originalFileName, newFileName) {
+  // Define the directory where the cache files are stored
   const cacheDir = "./filecache/";
 
+  // Create the full paths for the original and new files
   const originalPath = path.join(cacheDir, originalFileName);
   const newPath = path.join(cacheDir, newFileName);
 
+  // Rename the file using fs.rename
   fs.rename(originalPath, newPath, (err) => {
     if (err) {
+      // If an error occurs during the renaming process, log the error
       console.error("Error renaming file:", err);
       return;
     }
 
+    // If successful, log the renaming details
     console.log(`File ${originalFileName} renamed to ${newFileName}`);
   });
 }
--- Backend/express/functionals/transcriber.js
+++ Backend/express/functionals/transcriber.js
@@ -1,51 +1,91 @@
 const { spawn } = require("child_process")
 
+
+/**
+ * Calls the multimedia transcription process using a Python script.
+ *
+ * @param {string} file - The file name for transcription.
+ * @param {string} language - The language for transcription.
+ * @returns {Promise<string>} - A promise that resolves with the transcription data.
+ */
 function callMultimedia(file, language) {
     return new Promise((resolve, reject) => {
+      // Spawn a new Python process for multimedia transcription
       const ml = spawn("python", ["-c", `from ml import *; remote("filecache/"+"${file}", "${language}")`])
+
+      // Log a message when transcription starts
       console.log("Transcription started");
+
+      // Variable to store the output data
       var dataOUT = '';
-  
+
+      // Set the encoding for stdout
       ml.stdout.setEncoding("utf-8");
+
+      // Event listener for data received on stdout
       ml.stdout.on('data', (data) => {
         dataOUT += data.toString();
       });
-  
+      
+      // Event listener for data received on stderr (error stream)
       ml.stderr.on('data', (data) => {
         console.log(`stderr: ${data}`);
       });
   
+      // Event listener for when the process closes
       ml.on('close', (code) => {
         if (code) {
+          // If the process closes with an error, reject the promise
           console.log("Child died on error " + code);
           reject(`Error in transcription: ${code}`);
         } else {
+          // If the process closes successfully, resolve the promise
           resolve(dataOUT);
         }
       });
     });
   }
   
+
+/**
+ * Calls the text transcription process using a Python script.
+ *
+ * @param {string} file - The file name for transcription.
+ * @param {string} language - The language for transcription.
+ * @returns {Promise<string>} - A promise that resolves with the transcription data.
+ */  
 function callText(file, language) {
     return new Promise((resolve, reject) => {
+      // Spawn a new Python process for text transcription
       const ml = spawn("python", ["-c", `from ml import *; remote_translate("filecache/"+"${file}", "${language}")`])
+
+      // Log a message when transcription starts
       console.log("Transcription started");
+
+      // Variable to store the output data
       var dataOUT = '';
-  
+      
+      // Set the encoding for stdout
       ml.stdout.setEncoding("utf-8");
+
+      // Event listener for data received on stdout
       ml.stdout.on('data', (data) => {
         dataOUT += data.toString();
       });
-  
+      
+      // Event listener for data received on stderr (error stream)
       ml.stderr.on('data', (data) => {
         console.log(`stderr: ${data}`);
       });
-  
+      
+      // Event listener for when the process closes
       ml.on('close', (code) => {
         if (code) {
+          // If the process closes with an error, reject the promise
           console.log("Child died on error " + code);
           reject(`Error in transcription: ${code}`);
         } else {
+          // If the process closes successfully, resolve the promise
           resolve(dataOUT);
         }
       });
